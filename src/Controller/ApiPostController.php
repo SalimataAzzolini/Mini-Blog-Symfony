4b75c590a6522757dbc02fee0c1a7b8b8b783629
Salimata Azzolini <100932041+SalimataAzzolini@users.noreply.github.com>
2023-07-21 10:36:06 +0200

    Update ApiPostController.php

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -22,49 +22,29 @@ class ApiPostController extends AbstractController
      */
     public function index(ArticleRepository $articleRepository): Response
     {
-        // $articles = $articleRepository->findAll();
-
-        // $articlesNormalizes = $normalizer->normalize($articles, null, ['groups' => 'article:read']);
-
-
-        // dd($articlesNormalizes);
-        // $json = json_encode($articlesNormalizes);
-
-        // $json = $serializer->serialize($articles, 'json', ['groups' => 'article:read']);
-
-        // dd($json, $articlesNormalizes);
+         $articles = $articleRepository->findAll();
         
-        // $response = new JsonResponse($json, 200, [], true); possible d'écrire ça et d'effacer le content -type
-        // $response = $this->json($articles, 200, [], ['groups' => 'article:read']); remplace le normalize et le content-type 
-        //et l'encodage et enlever le normalize et $normalise dans les params
-        // $response = new Response($json, 200, [
-        //     "Content-Type" => "application/json"
-        // ]);
-
-       
-        //Possible de réduire tout le code en une seule ligne enleve aussi les params normalize
         return $this->json($articleRepository->findAll(), 200, [], ['groups' => 'article:read']);
         
-        // return $response;
     }
 
     /**
      * @Route("/api/post", name="app_api_post", methods={"POST"})
      */
-    // public function store(Request $request, SerializerInterface $serializer, EntityManagerInterface)
+     public function store(Request $request, SerializerInterface $serializer, EntityManagerInterface)
     
-    // {
+     {
     
-    //     $jsonRecu = $request->getContent();
-    //     $article = $serializer->deserialize($jsonRecu, Article::class, 'json');
+        $jsonRecu = $request->getContent();
+        $article = $serializer->deserialize($jsonRecu, Article::class, 'json');
 
-    //     $article->setCreatedAt(new \DateTime());
+        $article->setCreatedAt(new \DateTime());
         
-    //     $em->persist($article);
-    //     $em->flush();
-    //     dd($article);
+        $em->persist($article);
+        $em->flush();
+    
 
-    //     return $this->json($article, 201, ['groups' => 'article:read']);
+       return $this->json($article, 201, ['groups' => 'article:read']);
 
-    // }
+    }
 }
